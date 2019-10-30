a26324d3cf5b27c3cf6abbf3536d65f79a120ece
Kate <mercedes.catherine.salazar@gmail.com>
2019-10-30 23:24:49 +0100

    Valgrind call is now folded and numbering all...
    ... lines.

--- utils/ci/valgrind-worker.sh
+++ utils/ci/valgrind-worker.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+#
+# Copyright 2019 Mercedes Catherine Salazar
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# utils/ci/valgrind-worker.sh: This should run debug cases
+# demonstrating leak bugs as shown by `valgrind`, if applicable.
+#
+
 
 until false
 do
@@ -30,8 +49,20 @@ do
 		fi
 #		rm "${binary}" || break
 		make all || continue
-		valgrind --leak-check=full --show-leak-kinds=all --num-callers=24 "${binary}" run tests 2>&1 | nl >valgrind_report || (echo 'valgrind returned bad' && continue)
-		mv ./valgrind_report /var/www/html/valgrind/amara/${branch}/valgrind_report || (echo 'mv returned bad' && continue)
+		valgrind \
+				--leak-check=full \
+				--show-leak-kinds=all \
+				--num-callers=24 \
+				"${binary}"\
+				run tests \
+				2>&1 | \
+				nl -b a >valgrind_report || ( \
+				echo 'valgrind returned bad' && \
+				continue)
+		mv \
+				./valgrind_report \
+				/var/www/html/valgrind/amara/${branch}/valgrind_report || ( \
+				echo 'mv returned bad' && continue)
 		date
 		echo 'sleeping 10'
 		sleep 10
