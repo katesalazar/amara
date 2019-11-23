katesalazar/amara

commit 113d371b0d2b5d4a2f942d55aefd2cc915b225a6

Removed several leak bugs.
Leaks report (for `run tests`), `valgrind`:

* Reserved memory in use at exit down to 66919 from 149898 bytes
  (-55.36%).
* Allocations up to 20653 from 19148 (+7.86%).
* Frees up to 19346 from 12863 (+50.40%).
* Total bytes allocated up to 619507 from 596942 bytes (+3.78%).
* Total leak incidents down to 1269 from 5976 (-78.76%).
* Definitely lost down to 32865 from 53538 bytes (-38.95%).
* Indirectly lost down to 14836 from 77142 bytes (-80.77%).
* Still reachable stays in 19218 from 19218 bytes.
* Error contexts down to 440 from 2073 (-78.77%).

Coverage data (`run tests` plus some other calls (refer the `ci`
branch for more information, basically:
* `run tests` itself.
* `run tests --no-banner`.
* `run tests SOMETHING_ELSE`.
* `run app /PATH/TO/AN/APP`
* `run app /PATH/TO/AN/APP --no-banner`.
* `run app /PATH/TO/AN/EMPTY/DIRECTORY/`.
* `run app /PATH/TO/A/MISSING/DIRECTORY/`.
* possibly others)):
* Lines coverage up +0.28% from 95.39% (20452/21441) to 95.66%
  (21830/22821).
* Functions coverage up +0.31% from 97.31% (1373/1411) to 97.61%
  (1471/1507)
* Branches coverage up +0.21% from 76.82% (1352/1760) to 76.98%
  (1418/1842).

Starting to move user code out of the Bison symbols table, because
single responsibility principles.

Use more trivially printable values for some enumerated types' values,
for easier debugging.

My Xcode installation is now unable to use the project anymore, for
some reason yet to fix. However I'm using `lldb` from the console
without troubles.

Remove Homebrew from the mix, for now.

src/arn/app_runner.c
@@ -245,6 +245,8 @@ run_app_main_doc_exists(
 	rtg_doc_out_of_stt_doc_ret_ =
 			rtg_doc_out_of_stt_doc(minia_bison_main_ret_);
 
+	stt_node_destructor(minia_bison_main_ret_);
+
 #ifdef DUMP_FLOW_TO_STDERR
 	fprintf(stderr, "app_runner:225\n");
 #endif

src/stt/stt_condition.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Mercedes Catherine Salazar
+ * Copyright 2018-2019 Mercedes Catherine Salazar
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -189,9 +189,13 @@ stt_condition_equality(const stt_condition * c0, const stt_condition * c1)
 	equality_ = stt_expression_equality(
 			c0->right_hand_side_expression_,
 			c1->right_hand_side_expression_);
-	assertion(equality_ == AMARA_BOOLEAN_TRUE);
+	if (equality_ == AMARA_BOOLEAN_FALSE) {
+		returning_ = AMARA_BOOLEAN_FALSE;
+	} else {
+		assertion(equality_ == AMARA_BOOLEAN_TRUE);
 
-	returning_ = AMARA_BOOLEAN_TRUE;
+		returning_ = AMARA_BOOLEAN_TRUE;
+	}
 
 	/*
 
@@ -225,4 +229,10 @@ stt_condition_equality(const stt_condition * c0, const stt_condition * c1)
 	*/
 
 	return returning_;
+}
+
+amara_boolean
+stt_conditions_equality(const stt_condition * c0, const stt_condition * c1)
+{
+	return stt_condition_equality(c0, c1);
 }

src/stt/stt_condition_tests.c
@@ -69,6 +69,9 @@ stt_condition_example_simple_condition()
 			returning_);
 #endif
 
+	stt_expression_destructor(left_hand_side_expression_);
+	stt_expression_destructor(right_hand_side_expression_);
+
 	return returning_;
 }
 
@@ -107,6 +110,74 @@ assert_expectations_on_stt_condition_example_simple_true_condition(
 	/*   TODO: Must reverse. */
 	assert_expectations_on_stt_condition_example_simple_condition(
 			condition);
+}
+
+#endif
+
+/** `0 < 0`, which is a false condition. */
+stt_condition *
+stt_condition_example_simple_false_condition(void)
+{
+	stt_expression * left_hand_side_expression_;
+	stt_expression * right_hand_side_expression_;
+	stt_condition * returning_;
+
+	left_hand_side_expression_ = stt_expression_example_natural_zero();
+#ifndef NDEBUG
+	assertion(left_hand_side_expression_ != NULL);
+	assert_expectations_on_stt_expression_example_natural_zero(
+			left_hand_side_expression_);
+#endif
+
+	right_hand_side_expression_ = stt_expression_example_natural_zero();
+#ifndef NDEBUG
+	assertion(right_hand_side_expression_ != NULL);
+	assert_expectations_on_stt_expression_example_natural_zero(
+			right_hand_side_expression_);
+#endif
+
+	returning_ = stt_condition_default_constructor();
+#ifndef NDEBUG
+	assertion(returning_ != NULL);
+	assertion(returning_->type_ == STT_CONDITION_TYPE_INVALID);
+#endif
+
+	stt_condition_initialize_less_than(
+			returning_, left_hand_side_expression_,
+			right_hand_side_expression_);
+#ifndef NDEBUG
+	assertion(returning_->type_ == STT_CONDITION_TYPE_LESS_THAN);
+	assert_expectations_on_stt_expression_example_natural_zero(
+			returning_->left_hand_side_expression_);
+	assert_expectations_on_stt_expression_example_natural_zero(
+			returning_->right_hand_side_expression_);
+#endif
+
+#ifndef NDEBUG
+	assert_expectations_on_stt_condition_example_simple_false_condition(
+			returning_);
+#endif
+
+	stt_expression_destructor(left_hand_side_expression_);
+	stt_expression_destructor(right_hand_side_expression_);
+
+	return returning_;
+}
+
+#ifndef NDEBUG
+
+void
+assert_expectations_on_stt_condition_example_simple_false_condition(
+		const stt_condition * condition)
+{
+	assertion(condition != NULL);
+	assertion(condition->type_ == STT_CONDITION_TYPE_LESS_THAN);
+	assertion(condition->left_hand_side_expression_ != NULL);
+	assert_expectations_on_stt_expression_example_natural_zero(
+			condition->left_hand_side_expression_);
+	assertion(condition->right_hand_side_expression_ != NULL);
+	assert_expectations_on_stt_expression_example_natural_zero(
+			condition->right_hand_side_expression_);
 }
 
 #endif
@@ -169,6 +240,9 @@ stt_condition_example_simple_gt_true_condition(void)
 			returning_->right_hand_side_expression_);
 #endif
 
+	stt_expression_destructor(left_hand_side_expression_);
+	stt_expression_destructor(right_hand_side_expression_);
+
 #ifndef NDEBUG
 	assert_expectations_on_stt_condition_example_simple_gt_true_condition(
 			returning_);
@@ -229,17 +303,40 @@ stt_condition_create_and_destroy_less_than_examples()
 }
 
 void
-stt_condition_create_and_destroy_greater_than_examples()
+stt_condition_create_and_destroy_greater_than_example_0()
 {
 	stt_condition * condition_;
 
 	condition_ = stt_condition_example_simple_gt_true_condition();
+	forced_assertion(condition_ != NULL);
 #ifndef NDEBUG
 	assert_expectations_on_stt_condition_example_simple_gt_true_condition(
 			condition_);
 #endif
 
 	stt_condition_destructor(condition_);
+}
+
+void
+stt_condition_create_and_destroy_greater_than_example_1()
+{
+	stt_condition * condition_;
+
+	condition_ = stt_condition_example_simple_greater_than_condition();
+	forced_assertion(condition_ != NULL);
+#ifndef NDEBUG
+	assert_expectations_on_stt_condition_example_simple_greater_than_condition(
+			condition_);
+#endif
+
+	stt_condition_destructor(condition_);
+}
+
+void
+stt_condition_create_and_destroy_greater_than_examples()
+{
+	stt_condition_create_and_destroy_greater_than_example_0();
+	stt_condition_create_and_destroy_greater_than_example_1();
 }
 
 void

src/stt/stt_condition_tests.h
@@ -52,6 +52,21 @@ assert_expectations_on_stt_condition_example_simple_true_condition(
 
 #endif
 
+/**  A simple true condition. */
+stt_condition *
+stt_condition_example_simple_false_condition(void)
+__attribute__((warn_unused_result))
+;
+
+#ifndef NDEBUG
+
+void
+assert_expectations_on_stt_condition_example_simple_false_condition(
+		const stt_condition * condition)
+;
+
+#endif
+
 /**  A simple _less than_ true condition. */
 stt_condition *
 stt_condition_example_simple_lt_true_condition(void)

commit 167a3336ffa5c7ddbca71f63463641cbc29c8dae

Fix broken examples `0_...` and `1_...`.
Fix examples `0_...` and `1_...` broken by revision `113d371b0d2b5d4`
of 29th Sep 2019.

Organize headers in `src/stt/stt_condition_tests.(c|h)`. Provide
corresponding unit tests for new code paths.

src/arn/app_runner.c
@@ -881,10 +881,8 @@ run_operation(const rtg_operation * operation,
 			assertion(expression_evaluated_value_->string_->value_ !=
 					NULL);
 #endif
-			/*
 			printf("%s",
 			       expression_evaluated_value_->string_->value_);
-			*/
 			} else {
 				forced_assertion_two(expression_evaluated_value_->type_ == ARN_VALUE_TYPE_ANONYMOUS_ASSIGNED_NATURAL,
 						"unexpected behavior, app_runner.c: 761");
@@ -897,10 +895,8 @@ run_operation(const rtg_operation * operation,
 				assertion_two(expression_evaluated_value_->natural_->raw_->value_ != NULL,
 						"app_runner.c: 766");
 #endif
-				/*
 				printf("%s",
 				       expression_evaluated_value_->natural_->raw_->value_);
-				*/
 			}
 		}
 		forced_assertion(operation->type_ == RTG_OPERATION_TYPE_PRINT);

src/stt/stt_condition.c
@@ -178,8 +178,16 @@ stt_condition_equality(const stt_condition * c0, const stt_condition * c1)
 
 	assertion(c0->type_ == c1->type_);  /* XXX */
 
-	assertion(c0->type_ == STT_CONDITION_TYPE_LESS_THAN);
-	assertion(c1->type_ == STT_CONDITION_TYPE_LESS_THAN);
+	if (c0->type_ == STT_CONDITION_TYPE_LESS_THAN) {
+
+		assertion(c0->type_ == STT_CONDITION_TYPE_LESS_THAN);
+		assertion(c1->type_ == STT_CONDITION_TYPE_LESS_THAN);
+	} else {
+		forced_assertion(c0->type_ == STT_CONDITION_TYPE_GREATER_THAN);  /* XXX */
+
+		assertion(c0->type_ == STT_CONDITION_TYPE_GREATER_THAN);
+		assertion(c1->type_ == STT_CONDITION_TYPE_GREATER_THAN);
+	}
 
 	equality_ = stt_expression_equality(
 			c0->left_hand_side_expression_,

src/stt/stt_condition_tests.c
@@ -25,9 +25,8 @@
 /*   For owned functions prototypes. */
 #include "stt_condition_tests.h"
 
-/** `0 < 1`, which is a true condition. */
 stt_condition *
-stt_condition_example_simple_condition()
+stt_condition_example_simple_lt_true_condition(void)
 {
 	stt_expression * left_hand_side_expression_;
 	stt_expression * right_hand_side_expression_;
@@ -78,7 +77,7 @@ stt_condition_example_simple_condition()
 #ifndef NDEBUG
 
 void
-assert_expectations_on_stt_condition_example_simple_condition(
+assert_expectations_on_stt_condition_example_simple_lt_true_condition(
 		const stt_condition * condition)
 {
 	assertion(condition != NULL);
@@ -93,12 +92,10 @@ assert_expectations_on_stt_condition_example_simple_condition(
 
 #endif
 
-/**  A simple true condition. */
 stt_condition *
 stt_condition_example_simple_true_condition(void)
 {
-	/*   TODO: Must reverse. */
-	return stt_condition_example_simple_condition();
+	return stt_condition_example_simple_lt_true_condition();
 }
 
 #ifndef NDEBUG
@@ -107,14 +104,30 @@ void
 assert_expectations_on_stt_condition_example_simple_true_condition(
 		const stt_condition * condition)
 {
-	/*   TODO: Must reverse. */
-	assert_expectations_on_stt_condition_example_simple_condition(
+	assert_expectations_on_stt_condition_example_simple_lt_true_condition(
 			condition);
 }
 
 #endif
 
-/** `0 < 0`, which is a false condition. */
+stt_condition *
+stt_condition_example_simple_condition(void)
+{
+	return stt_condition_example_simple_true_condition();
+}
+
+#ifndef NDEBUG
+
+void
+assert_expectations_on_stt_condition_example_simple_condition(
+		const stt_condition * condition)
+{
+	assert_expectations_on_stt_condition_example_simple_true_condition(
+			condition);
+}
+
+#endif
+
 stt_condition *
 stt_condition_example_simple_false_condition(void)
 {
@@ -178,26 +191,6 @@ assert_expectations_on_stt_condition_example_simple_false_condition(
 	assertion(condition->right_hand_side_expression_ != NULL);
 	assert_expectations_on_stt_expression_example_natural_zero(
 			condition->right_hand_side_expression_);
-}
-
-#endif
-
-stt_condition *
-stt_condition_example_simple_lt_true_condition(void)
-{
-	/*   TODO: Must reverse. */
-	return stt_condition_example_simple_true_condition();
-}
-
-#ifndef NDEBUG
-
-void
-assert_expectations_on_stt_condition_example_simple_lt_true_condition(
-		const stt_condition * condition)
-{
-	/*   TODO: Must reverse. */
-	assert_expectations_on_stt_condition_example_simple_true_condition(
-			condition);
 }
 
 #endif
@@ -347,7 +340,34 @@ stt_condition_create_and_destroy_examples()
 }
 
 void
+stt_condition_equality_test_0()
+{
+	stt_condition * condition_zero_;
+	stt_condition * condition_one_;
+	amara_boolean result_;
+
+	condition_zero_ = stt_condition_example_simple_gt_true_condition();
+	forced_assertion(condition_zero_ != NULL);
+
+	condition_one_ = stt_condition_example_simple_gt_true_condition();
+	forced_assertion(condition_one_ != NULL);
+
+	result_ = stt_conditions_equality(condition_zero_, condition_one_);
+	forced_assertion(result_ == AMARA_BOOLEAN_TRUE);
+
+	stt_condition_destructor(condition_zero_);
+	stt_condition_destructor(condition_one_);
+}
+
+void
+stt_condition_equality_tests()
+{
+	stt_condition_equality_test_0();
+}
+
+void
 stt_condition_tests()
 {
 	stt_condition_create_and_destroy_examples();
+	stt_condition_equality_tests();
 }

src/stt/stt_condition_tests.h
@@ -19,19 +19,22 @@
 #ifndef __AMARA__SYNTAX_TREE__CONDITION_TESTS__H__
 #define __AMARA__SYNTAX_TREE__CONDITION_TESTS__H__
 
+#include "../definitions.h"
+
 /*   For `stt_condition`. */
 #include "stt_condition.h"
 
-/**  `0 less than 1`. */
+/**  `0 < 1` (zero less than one), which is a simple true _less than_
+ * condition. */
 stt_condition *
-stt_condition_example_simple_condition(void)
+stt_condition_example_simple_lt_true_condition(void)
 __attribute__((warn_unused_result))
 ;
 
 #ifndef NDEBUG
 
 void
-assert_expectations_on_stt_condition_example_simple_condition(
+assert_expectations_on_stt_condition_example_simple_lt_true_condition(
 		const stt_condition * condition)
 ;
 
@@ -52,7 +55,23 @@ assert_expectations_on_stt_condition_example_simple_true_condition(
 
 #endif
 
-/**  A simple true condition. */
+/**  A simple condition. */
+stt_condition *
+stt_condition_example_simple_condition(void)
+__amara__warn_unused_result__
+;
+
+#ifndef NDEBUG
+
+/**  A simple condition. */
+void
+assert_expectations_on_stt_condition_example_simple_condition(
+		const stt_condition * condition)
+;
+
+#endif
+
+/** `0 < 0`, which is a false condition. */
 stt_condition *
 stt_condition_example_simple_false_condition(void)
 __attribute__((warn_unused_result))
@@ -62,21 +81,6 @@ __attribute__((warn_unused_result))
 
 void
 assert_expectations_on_stt_condition_example_simple_false_condition(
-		const stt_condition * condition)
-;
-
-#endif
-
-/**  A simple _less than_ true condition. */
-stt_condition *
-stt_condition_example_simple_lt_true_condition(void)
-__attribute__((warn_unused_result))
-;
-
-#ifndef NDEBUG
-
-void
-assert_expectations_on_stt_condition_example_simple_lt_true_condition(
 		const stt_condition * condition)
 ;
 