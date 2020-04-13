12227afe07b920d383a60ac87fa6ab351530393d
Kate <mercedes.catherine.salazar@gmail.com>
2020-04-13 14:44:27 +0200

    wip

--- src/rtg/rtg_operation_arg.c
+++ src/rtg/rtg_operation_arg.c
@@ -655,7 +655,7 @@ rtg_operation_arg_out_of_stt_operation_arg(
 		sub_ret_->operation_ = NULL;
 		sub_ret_->type_ = RTG_OPERATION_ARG_TYPE_EXPRESSION;
 	} else {
-		forced_assertion(operation_arg->node_->type_ == STT_NODE_TYPE_FUNCTION_CALL);
+		forced_assertion(operation_arg->node_->type_ == STT_NODE_TYPE_NAMED_FUNCTION_CALL);
 #ifndef NDEBUG
 		stt_node_assertion_clean_function_call_node(operation_arg->node_);
 		assertion(operation_arg->node_->sub_function_call_ != NULL);
